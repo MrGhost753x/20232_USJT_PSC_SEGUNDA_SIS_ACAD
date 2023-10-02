fb565dd27317a4bdb7da403e896ee327978ad59b
Gustavo Baeza <gustavobaeza.2001@hotmail.com>
2023-10-02 20:58:20 -0300

    Ap11(Login): campos login e senha

---
+++ src/main/java/br/com/baeza/sis_acad/_usjt_psc_segunda_sis_acad/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.baeza.sis_acad._usjt_psc_segunda_sis_acad;
+
+/**
+ *
+ * @author 82319972
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
